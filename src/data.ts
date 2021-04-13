8cf1ce33e8e84b0c42c6075d7ddfcce188d83428
sergeichestakov <schestakov@ucdavis.edu>
2021-04-13 10:23:52 -0400

    Add provided sample JSON data

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+const data = {
+  status: "REQUEST",
+  startDate: "2021-04-07T00:48:50.494Z",
+  endDate: "2021-04-15T00:48:50.494Z",
+  startTime: "9:00 am",
+  endTime: "10:00 pm",
+  guestName: "Jennifer",
+  rooms: 10,
+  numberOfGuests: 5,
+  listingType: "Penthouse Office",
+  payout: 8870,
+  guestNumber: "+19255865543",
+  serviceFeePercentage: 10,
+  profilePicture:
+    "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+};
+
+export default data;
