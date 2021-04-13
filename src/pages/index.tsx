fff24ad1d7fe5b36d25c311d25f9d9cc5cfebb5a
sergeichestakov <schestakov@ucdavis.edu>
2021-04-13 11:01:36 -0400

    Add tabs boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { Heading } from "@chakra-ui/react";
+import {
+  Heading,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tabs,
+  Tab,
+} from "@chakra-ui/react";
 import Container from "../components/Container";
 import DarkModeSwitch from "../components/DarkModeSwitch";
 import SearchBar from "../components/SearchBar";
@@ -11,6 +18,25 @@ export default function Index() {
         Requests & Reservations
       </Heading>
       <SearchBar />
+      <Tabs>
+        <TabList>
+          <Tab>Requests</Tab>
+          <Tab>Accepted</Tab>
+          <Tab>Rejected</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>Requests</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Accepted</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Rejected</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
       <DarkModeSwitch />
     </Container>
   );
